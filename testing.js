ad0bcd9b873a7fca3d74bc03e11bf100a2d25c64
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2020-11-14 22:51:42 +0900

    Update testing.js

--- testing.js
+++ testing.js
@@ -4,30 +4,12 @@ const twt_a5tabot = require('./controllers/twitter/twt_a5tabot');
 const twt_notkwayes = require('./controllers/twitter/twt_not_kwayes');
 const cron = require('node-cron');
 
-async function FSQ_BuFai7an_Autolike() {
-	const value = await likeUnliked({ limit: 60 });
-	console.log(value);
-}
+// twt_bufai7an.likeHashtag({ q: '#السعودية', number_of_likes: 10 });
 
-async function TWT_BuFai7anLiker() {
-	const hashtag = '#السعودية';
-	const number_of_likes = 10;
-	twt_bufai7an.likeHashtag({ q: hashtag, number_of_likes: number_of_likes });
-	// twt_bufai7an.getRateLimits();
-}
 
-async function TWT_BuFai7anRandomLiker() {
-	const number_of_likes = 2;
-	const number_of_hashtags = 5;
-	// number_of_likes * number_of_hashtags
+// twt_bufai7an.likeRandomHashtags({ number_of_likes: 2, number_of_hashtags: 5 });
 
-	twt_bufai7an.likeRandomHashtags({ number_of_likes: number_of_likes, number_of_hashtags: number_of_hashtags });
-}
-
-function TWT_BuFai7anUpdateHashtags() {
-	twt_bufai7an.updateHashtags();
-}
-
+// twt_bufai7an.updateHashtags();
 // twt_bufai7an.addHashtagUsersToList();
 
 fsq_bufai7an.likeUnliked({ limit: 60 });
