07b7601ff82b407c6082e67c28ac005bae64f30b
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2020-11-16 16:47:16 +0900

    disable not kwayes

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ cron.schedule('30 * * * *', () => {
 	twt_bufai7an.updateHashtags();
 	twt_bufai7an.addHashtagUsersToList({ number_of_likes: 4, number_of_hashtags: 10, 'list_id': '1327297142831026176' });
 	twt_a5tabot.addHashtagUsersToList({ number_of_likes: 4, number_of_hashtags: 10, 'list_id': '1327582294094340096' });
-	twt_notkwayes.addHashtagUsersToList({ number_of_likes: 4, number_of_hashtags: 10, 'list_id': '1327585663068241920' });
-	twt_notkwayes.raidHaaHashtags({ number_of_replies: 4, number_of_hashtags: 10 });
+	// twt_notkwayes.addHashtagUsersToList({ number_of_likes: 4, number_of_hashtags: 10, 'list_id': '1327585663068241920' });
+	// twt_notkwayes.raidHaaHashtags({ number_of_replies: 4, number_of_hashtags: 10 });
 });
 
 cron.schedule('*/10 * * * * *', () => {
 	console.log('Running Every 10 Seconds');
 	twt_a5tabot.answer();
-	twt_notkwayes.answer();
+	// twt_notkwayes.answer();
 });
 
 cron.schedule('*/10 * * * *', () => {
