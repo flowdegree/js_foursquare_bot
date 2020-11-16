4136a943ac45aea70958d91da143440fda978da7
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2020-11-16 16:52:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+// Scheduler Samples https://crontab.guru/#*_*_*_*_*
+const cron = require('node-cron');
 const fsq_bufai7an = require('./controllers/foursquare/fsq_bufai7an');
 const twt_bufai7an = require('./controllers/twitter/twt_bufai7an');
 const twt_a5tabot = require('./controllers/twitter/twt_a5tabot');
 const twt_notkwayes = require('./controllers/twitter/twt_not_kwayes');
-const cron = require('node-cron');
+
 
 async function FSQ_BuFai7an_Autolike() {
 	const value = await fsq_bufai7an.likeUnliked({ limit: 60 });
 	console.log(value);
 }
 
-cron.schedule('30 * * * *', () => {
+cron.schedule('*/30 * * * *', () => {
 	// once per hour, should not exceed 41 likes
-	console.log('Running Every 1 hour @ 30 minutes');
+	console.log('Running Every 30 minutes');
 	twt_bufai7an.updateHashtags();
 	twt_bufai7an.addHashtagUsersToList({ number_of_likes: 4, number_of_hashtags: 10, 'list_id': '1327297142831026176' });
 	twt_a5tabot.addHashtagUsersToList({ number_of_likes: 4, number_of_hashtags: 10, 'list_id': '1327582294094340096' });
