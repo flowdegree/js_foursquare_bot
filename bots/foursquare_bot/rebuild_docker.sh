4ef2b96bd58235d7124123d619fee3b9d0f40bb5
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2022-04-14 23:14:34 +0300

    Create rebuild_docker.sh

---
+++ bots/foursquare_bot/rebuild_docker.sh
@@ -0,0 +1,5 @@
+docker stop js_foursquare_bot
+docker rm js_foursquare_bot
+docker images rm mo9a7i/js_foursquare_bot
+docker build -t mo9a7i/js_foursquare_bot .
+docker run --name js_foursquare_bot -d mo9a7i/js_foursquare_bot
