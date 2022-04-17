ba2889a100b01db76426b4e4be63eaaa32095edb
mo9a7i <mohannad.otaibi@gmail.com>
2022-04-18 02:34:18 +0300

    commit pull review merge

--- bots/github_bot/index.js
+++ bots/github_bot/index.js
@@ -9,7 +9,6 @@ const octokit = new Octokit({
   auth: config.github.mo9a7i.token,
 });
 
-
 const constants = {
 	owner: "6degrees",
 	repo: "time_now",
@@ -17,55 +16,38 @@ const constants = {
 //commit every minute
 async function commit_time(branch_name = undefined){
 	try {
-		const timenow =  Base64.encode(`${Date()}`);
 
-		const path = `time_now.txt`;
+		const path = `README.md`;
+
 		let result = await octokit.repos.getContent({
 			...constants,
+			ref: 'newest_time',
 			path,
 		});
 		
 		const sha = result?.data?.sha;
-
-
-		
-		if(branch_name){
-			const branch_sha = await octokit.rest.git.getRef({
-				...constants,
-				ref: 'heads/main',
-			});
-
-			const sha = branch_sha?.data?.object?.sha;
-
-			console.log(sha);
-
-			result = await octokit.rest.git.createRef({
-				...constants,
-				ref: `refs/heads/${branch_name}`,
-				sha,	
-			});
-			constants.branch = branch_name;
-		}
-		
-		
+		const content = Base64.decode(result.data.content);
+		const new_content = content.replace(/\(\(.*\)\)/g, `(( ${Date()} ))`);
+		const encoded = Base64.encode(new_content);
+		//process.exit();
 
 		result = await octokit.repos.createOrUpdateFileContents({
 			...constants,
 			path,
 			message: `Update time to "${Date.now()}"`,
-			content: timenow,
+			branch: 'newest_time',
+			content: encoded,
 			sha,
 		});
 
-		if(branch_name){
-			await create_pull(branch_name);
-		}
-		
-		console.log(result || 500);
-	} catch (error) {
+		console.log(result ? "Success committing" : "failed comitting");
+
+		await create_pull('newest_time');
+	
+	} 
+	catch (error) {
 		console.error(error.response)
 	}
-	
 }
 
 async function create_issue(){
@@ -102,7 +84,6 @@ async function create_issue(){
 	  
 }
 
-
 async function create_pull(branch_name){
 	try {	
 		let result = await octokit.pulls.create({
@@ -114,30 +95,46 @@ async function create_pull(branch_name){
 		});
 		console.log("pull_number", result.data.number);
 
-		result = await octokit.pulls.createReview({
-			...constants,
-			pull_number: result.data.number,
-
-		})
-		result = await octokit.pulls.merge({
-			...constants,
-			pull_number: result.data.number,
-			commit_title: 'merging branches'
-		})
+		create_review(result.data.number);
+		console.log('done;');
+		
 
 	} catch (error) {
 		console.error(error?.response?.data?.errors);
 	}
 }
 
+async function create_review(pull_number){
+	try {		
+		result = await octokit.pulls.createReview({
+			...constants,
+			pull_number: pull_number,
+			body: 'looks fine',
+			event: 'COMMENT'
+		})
+		create_merge(pull_number);
+	} 
+	catch (error) {
+		console.error(error?.response?.data?.errors);
+	}
+}
 
-//create_issue();
-commit_time();
-commit_time('another_branch_ttt');
-//create_pull();
+async function create_merge(pull_number){
+	try {	
+		result = await octokit.pulls.merge({
+			...constants,
+			pull_number: pull_number,
+			commit_title: 'merging branches'
+		})
+	} 
+	catch (error) {
+		console.error(error?.response?.data?.errors);
+	}
+}
 
 
-cron.schedule('* * * * *', () => {
+
+cron.schedule('*/30 * * * *', () => {
 	try {
 		commit_time();
 	} catch (error) {
